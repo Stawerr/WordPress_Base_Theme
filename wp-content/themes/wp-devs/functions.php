96bbe965eff977a0e7681ea906df72055dd48099
Stawerr <renato-silva_16@hotmail.com>
2022-11-14 11:32:04 +0000

    base theme WP (JS added)

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -5,5 +5,7 @@ function wpdevs_load_scripts(){
     wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all');
     //Carregar as fonts
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null );
+    //Carregar o ficheiro Dropdown.JS (poderia ser o main.js)
+    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
