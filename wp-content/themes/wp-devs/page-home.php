6f5a91cd225bc2b557ed55e58aa7e35094d163d0
Stawerr <renato-silva_16@hotmail.com>
2022-11-16 10:52:52 +0000

    theme customizer category

--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -3,17 +3,23 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    <section class="hero" style="background-image: url('http://localhost/udemy/wp-content/uploads/2022/11/Foto_Vinhos.jpg')">
-                        <div class="overlay" style="min-height: 800px">
+                    <?php
+                        $hero_title = get_theme_mod('set_hero_title', 'Please, type some title');
+                        $hero_subtitle = get_theme_mod('set_hero_subtitle','Please, type some subtitle');
+                        $hero_button_link = get_theme_mod('set_hero_button_link','#');
+                        $hero_button_text = get_theme_mod('set_hero_button_text','Learn More');
+                        $hero_height = get_theme_mod('set_hero_height',800);
+                        $hero_background = wp_get_attachment_url( get_theme_mod(
+                                'set_hero_background'
+                        ));
+                    ?>
+                    <section class="hero" style="background-image: url('<?php echo $hero_background ?>')">
+                        <div class="overlay" style="min-height: <?php echo $hero_height ?>px">
                             <div class="container">
                                 <div class="hero-items">
-                                    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    </h1>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad atque eaque
-                                        exercitationem harum incidunt ipsam maxime nesciunt, nostrum possimus, quaerat,
-                                        quidem repellendus suscipit.
-                                    </p>
-                                    <a href="#">Learn More</a>
+                                    <h1><?php echo $hero_title ?></h1>
+                                    <p><?php echo nl2br($hero_subtitle)  ?></p>
+                                    <a href="<?php echo $hero_button_link ?>"><?php echo $hero_button_text ?></a>
                                 </div>
                             </div>
                         </div>
@@ -29,11 +35,15 @@
                         <div class="container">
                                 <?php
 
+                                $per_page = get_theme_mod('set_per_page',3);
+                                $category_include = get_theme_mod('set_category_include');
+                                $category_exclude = get_theme_mod('set_category_exclude');
+
                                 $args =array(
                                     'post_type' => 'post',
-                                    'posts_per_page' => 3,
-                                    'category__in' => array(6,5),
-                                    'category__not_in' => array(1)
+                                    'posts_per_page' => $per_page,
+                                    'category__in' => explode(',',$category_include),
+                                    'category__not_in' => explode(',',$category_exclude)
 
                                 );
                                 $postlist = new WP_Query( $args );
